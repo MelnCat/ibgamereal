c4db979091f4585e89df62469ab62499cf82ee21
MelnCat <melncatuwu@gmail.com>
2025-02-23 13:28:58 -0800

    f

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,10 +1,6 @@
 /// <reference types="vite-plugin-glsl/ext" />
 import type * as CSS from "csstype";
 
-declare module "glsl-canvas-js" {
-	export * from "glsl-canvas-js/dist/cjs/glsl.d.ts";
-}
-
 declare module "csstype" {
 	interface Properties {
 		[index: `--${string}`]: string | number;
---
+++ src/glsl-canvas-js.d.ts
@@ -0,0 +1,3 @@
+declare module "glsl-canvas-js" {
+	export * from "glsl-canvas-js/dist/cjs/glsl.d.ts";
+}
